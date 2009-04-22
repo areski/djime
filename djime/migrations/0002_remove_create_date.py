0e27f7cefffe5c27a6274679961455bad3f4accf
Mikkel Hoegh <mikkel@hoegh.org>
2009-04-22 21:22:14 +0200

    A bit of documentation to migration 0002.

--- djime/migrations/0002_remove_create_date.py
+++ djime/migrations/0002_remove_create_date.py
@@ -1,14 +1,13 @@
-
 from south.db import db
 from django.db import models
 from djime.models import *
 import datetime
 
 class Migration:
-    
+    """ Simple migration deleting the create_date field. """
     def forwards(self):
         db.delete_column('djime_timeslice', 'create_date')
-    
+
     def backwards(self):
         db.add_column('djime_timeslice', 'create_date',
                       models.DateField(default=datetime.date.today())),
