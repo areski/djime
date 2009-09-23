b1bec3553aaa066c9e004faebd5be51346120f65
Jakob Torp Svendsen <jakob@revealit.dk>
2009-09-23 10:01:00 +0200

    Forgot to commit the timesheet_timeslice_handler.

--- djime/util.py
+++ djime/util.py
@@ -21,3 +21,19 @@ def format_seconds(seconds):
     return '%02i:%02i' % (duration['hours'], duration['minutes'])
 
 
+def timesheet_timeslice_handler(timeslices):
+    if not timeslices:
+        return timeslices
+    timeslices = timeslices.order_by('task', 'note')
+    result = []
+    test = []
+    # Create a timeslice like object, that is unique.
+    temp_slice =  type('temp_slice', (object,), {'note': 0, 'task':0})
+    for timeslice in timeslices:
+        if timeslice.note == temp_slice.note and timeslice.task == temp_slice.task:
+            temp_slice.duration += timeslice.duration
+        else:
+            result.append(temp_slice)
+            temp_slice = timeslice
+    result.append(temp_slice)
+    return result[1:]
