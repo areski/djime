1c191878104622fb9be71d18083252fe845c972c
Jakob Torp Svendsen <jakob@revealit.dk>
2009-11-11 15:24:05 +0100

    Added some table sorting styles.

--- examples/pine/media/js/pine.js
+++ examples/pine/media/js/pine.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  $("table.djime").tablesorter();
+  /**
+   * Function to handle clases on td elements to keep even/odd styling
+   */
+  djime.tablesort = function() {
+    $("table.djime tr").each(function(index) {
+      if (index % 2 == 0) {
+        $(this).removeClass('odd').removeClass('even').addClass('even');
+      }
+      else {
+        $(this).removeClass('odd').removeClass('even').addClass('odd');
+      }
+    });
+  }
+  $("table.djime").addClass("tablesorter").tablesorter();
+  $("table.djime th").click(function() {
+    setTimeout('djime.tablesort()', 100)
+  });
 });
